5945837a7900d5e2aa40e49bd146049cc6031eb8
ajit-on-git <kumarajitp58@gmail.com>
2026-03-12 08:40:47 +0530

    fix

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create axios instance with default config
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://impact-reporting-backend.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
