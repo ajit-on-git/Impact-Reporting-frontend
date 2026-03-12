c65b241e77e12f9754c533cd3727010b878b17b4
ajit-on-git <kumarajitp58@gmail.com>
2026-03-12 08:47:43 +0530

    fix

--- src/services/api.js
+++ src/services/api.js
@@ -6,7 +6,7 @@ const API = axios.create({
   headers: {
     "Content-Type": "application/json",
   },
-  timeout: 10000, // 10 seconds timeout
+  // timeout: 10000, // 10 seconds timeout
 });
 
 /**
